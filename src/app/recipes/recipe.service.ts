c07c1cf9e623aba6887ab25baa29cc3928f76403
Scott Rabara <scottrabara@gmail.com>
2017-04-28 11:10:15 -0700

    Changed Recipe model to include Ingredients. Changed Recipe Service to instantiate Recipe boilerplate information with Ingredient Array

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,10 +7,12 @@ export class RecipeService {
 	private recipes: Recipe[] = [
 	new Recipe('A test recipe', 
 			   'Description', 
-			   'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
+			   'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+			   []),
 	new Recipe('Another test recipe', 
 			   'Another description', 
-			   'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/7/25/2/FNM_090112-Weeknight-Lasagna-Dinner-Recipe_s4x3.jpg.rend.hgtvcom.966.725.jpeg')
+			   'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/7/25/2/FNM_090112-Weeknight-Lasagna-Dinner-Recipe_s4x3.jpg.rend.hgtvcom.966.725.jpeg',
+			   [])
 	];
 
 
