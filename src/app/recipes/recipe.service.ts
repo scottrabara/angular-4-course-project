45641f7c989a2de30abea27e362f382ab42a5c03
Scott Rabara <scottrabara@gmail.com>
2017-04-28 11:36:23 -0700

    Changed Recipe Detail to output Ingredients array in Recipe Model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,21 @@
 import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 
 	private recipes: Recipe[] = [
-	new Recipe('A test recipe', 
-			   'Description', 
+	new Recipe('Chicken Soup', 
+			   'Delicious', 
 			   'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
-			   []),
-	new Recipe('Another test recipe', 
-			   'Another description', 
+			   [new Ingredient('Chicken', 2),
+			    new Ingredient('Red Soup', 1)]),
+	new Recipe('Lasagna', 
+			   'Italian!', 
 			   'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/7/25/2/FNM_090112-Weeknight-Lasagna-Dinner-Recipe_s4x3.jpg.rend.hgtvcom.966.725.jpeg',
-			   [])
+			   [new Ingredient('Lasagna Sheet', 2),
+			    new Ingredient('Beef', 10)])
 	];
 
 
