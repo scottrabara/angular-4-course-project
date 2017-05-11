559b325aa38ce5377c28c4aa66d0ee0345fab6fd
Scott Rabara <scottrabara@gmail.com>
2017-05-11 14:47:49 -0700

    Added firebase config to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyATUksMuODaWY8e39HgbLUTEsvwKrjJM74",
+    authDomain: "ng-recipe-book-srabara.firebaseapp.com",
+    databaseURL: "https://ng-recipe-book-srabara.firebaseio.com",
+    projectId: "ng-recipe-book-srabara",
+    storageBucket: "ng-recipe-book-srabara.appspot.com",
+    messagingSenderId: "915128900868"
+  }
 };
